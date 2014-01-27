356d45d2018525736b78807789c7453a6a98152e
Yu Sugawara <yusuga@me.com>
2014-01-27 18:27:35 +0900

    Add Example

---
+++ Example/YSStoreKitExample/YSStoreKitExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YSStoreKitExample
+//
+//  Created by Yu Sugawara on 2014/01/27.
+//  Copyright (c) 2014年 Yu Sugawara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
